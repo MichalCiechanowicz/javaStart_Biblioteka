b82fdaade7df396a8a4a781e88a10261f1289b40
MichalCiechanowicz <emgasuk@gmail.com>
2019-11-21 21:01:06 +0000

    start

---
+++ src/main/java/Biblioteka.java
@@ -0,0 +1,25 @@
+public class Biblioteka {
+
+    public static void main(String[] args) {
+
+        final String appName = "Biblioteka v1.0";
+
+        String tytul = "Piec lat karceru";
+        String autor = "Grzesiuk";
+        int dataWydania = 1986;
+        int iloscStron = 254;
+        String wydawca  = "PWN";
+        String isbn = "12947187";
+
+        System.out.println(appName);
+        System.out.println("Dostepne ksiazki w bibliotece");
+        System.out.println(tytul);
+        System.out.println(autor);
+        System.out.println(dataWydania);
+        System.out.println(iloscStron);
+        System.out.println(wydawca);
+        System.out.println(isbn);
+
+    }
+
+}
